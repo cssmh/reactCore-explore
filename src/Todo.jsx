e6fd1c30815edf61a0b8e46e73e8ea863a58d5a4
momen210 <touristmomen@gmail.com>
2023-12-20 11:38:32 +0600

    some changes

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,8 +1,12 @@
-export default function Todo({task, isDone}){
-   if(isDone){
-    return <li>Finished: {task}</li>
-   }
-   else{
-    return <li>Work on: {task}</li>
-   }
+// export default function Todo({task, isDone}){
+//    if(isDone){
+//     return <li>Finished: {task}</li>
+//    }
+//    else{
+//     return <li>Work on: {task}</li>
+//    }
+// }
+
+export default function Todo({ task, isDone }) {
+  return <li> {isDone ? "Completed" : "Work on"} {task}</li>;
 }
